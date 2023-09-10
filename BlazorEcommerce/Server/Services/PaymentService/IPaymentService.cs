712d7ea2b0cdc5d3cf18d32930a02465deb1c990
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-10 11:58:23 +0300

    Add Payment Service

---
+++ BlazorEcommerce/Server/Services/PaymentService/IPaymentService.cs
@@ -0,0 +1,10 @@
+using Stripe.Checkout;
+
+namespace BlazorEcommerce.Server.Services.PaymentService
+{
+    public interface IPaymentService
+    {
+        Task<Session> CreateCheckoutSession();
+        Task<ServiceResponse<bool>> FulfillOrder(HttpRequest request);
+    }
+}
