f33ba6df954a75f8725711dff71e1480eccd8756
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-11 12:05:08 +0300

    Address Controller

---
+++ BlazorEcommerce/Server/Controllers/AddressController.cs
@@ -0,0 +1,32 @@
+#region Usings
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+#endregion
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AddressController : ControllerBase
+    {
+        private readonly IAddressService _addressService;
+
+        public AddressController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<Address>>> GetAddress()
+        {
+            return await _addressService.GetAddress();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<Address>>> AddOrUpdateAddress(Address address)
+        {
+            return await _addressService.AddOrUpdateAddress(address);
+        }
+    }
+}
