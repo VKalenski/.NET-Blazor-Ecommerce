2c46b1fb45e92bb2c00d376874f79810a549fbd8
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-12 11:58:51 +0300

    Refactoring code structure

--- .NET-Blazor-6/Client/Program.cs
+++ .NET-Blazor-6/Client/Program.cs
@@ -1,18 +1,17 @@
-global using BlazorEcommerce.Shared;
-global using System.Net.Http.Json;
-global using BlazorEcommerce.Client.Services.ProductService;
-global using BlazorEcommerce.Client.Services.CategoryService;
-global using BlazorEcommerce.Client.Services.AuthService;
-global using Microsoft.AspNetCore.Components.Authorization;
-global using BlazorEcommerce.Client.Services.CartService;
-global using BlazorEcommerce.Client.Services.OrderService;
 global using BlazorEcommerce.Client.Services.AddressService;
+global using BlazorEcommerce.Client.Services.AuthService;
+global using BlazorEcommerce.Client.Services.CartService;
+global using BlazorEcommerce.Client.Services.CategoryService;
+global using BlazorEcommerce.Client.Services.OrderService;
+global using BlazorEcommerce.Client.Services.ProductService;
 global using BlazorEcommerce.Client.Services.ProductTypeService;
+global using BlazorEcommerce.Shared;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using System.Net.Http.Json;
 using BlazorEcommerce.Client;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Blazored.LocalStorage;
-using BlazorEcommerce.Client.Services.ProductTypeService;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
