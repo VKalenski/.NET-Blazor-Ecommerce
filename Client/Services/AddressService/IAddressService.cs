0586d3c165e547f2f0d3bbad317e1360469e5950
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-12 11:18:14 +0300

    Added and refactoring Services

---
+++ Client/Services/AddressService/IAddressService.cs
@@ -0,0 +1,8 @@
+namespace BlazorEcommerce.Client.Services.AddressService
+{
+    public interface IAddressService
+    {
+        Task<Address> GetAddress();
+        Task<Address> AddOrUpdateAddress(Address address);
+    }
+}
